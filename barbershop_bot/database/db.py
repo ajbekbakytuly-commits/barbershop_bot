99b8dd4c2f9ff8170f543ef74097bb90b6ccd682
Aibek <aibek@example.com>
2026-03-16 02:26:53 +0500

    aiba

--- barbershop_bot/database/db.py
+++ barbershop_bot/database/db.py
@@ -1,13 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("barbershop.db")
-cursor = conn.cursor()
+DB_NAME = "barbershop.db"
 
 
+# создание таблицы
 def init_db():
 
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS appointments(
+    CREATE TABLE IF NOT EXISTS appointments (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         service TEXT,
@@ -18,9 +21,14 @@ def init_db():
     """)
 
     conn.commit()
+    conn.close()
 
 
+# добавить запись
 def add_appointment(user_id, service, master, date, time):
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
 
     cursor.execute(
         "INSERT INTO appointments (user_id, service, master, date, time) VALUES (?, ?, ?, ?, ?)",
@@ -28,31 +36,49 @@ def add_appointment(user_id, service, master, date, time):
     )
 
     conn.commit()
+    conn.close()
 
 
-def get_user_appointments(user_id):
-
-    cursor.execute(
-        "SELECT service, master, date, time FROM appointments WHERE user_id=?",
-        (user_id,)
-    )
-
-    return cursor.fetchall()
-
-
+# проверить занято ли время
 def is_time_busy(master, date, time):
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
 
     cursor.execute(
         "SELECT * FROM appointments WHERE master=? AND date=? AND time=?",
         (master, date, time)
     )
 
-    return cursor.fetchone()
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return result is not None
 
 
+# записи пользователя
+def get_user_appointments(user_id):
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT service, master, date, time FROM appointments WHERE user_id=?",
+        (user_id,)
+    )
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
+
+
+# все записи (для админа)
 def get_all_appointments():
 
-    conn = sqlite3.connect("barbershop.db")
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
 
     cursor.execute(
